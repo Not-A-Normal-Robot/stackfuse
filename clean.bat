591330bad0a42aa614b13195d8bf4ce4e2f4bbc7
Ishaan Bhardwaj <sashlilac109@gmail.com>
2021-07-17 23:08:01 -0400

    Fixed build scripts

--- clean.bat
+++ clean.bat
@@ -1,11 +1,17 @@
-@del cambridge.love
-@del dist\windows\cambridge.exe
+@del stackfuse.love
+@del dist\windows\stackfuse.exe
 @del dist\windows\SOURCES.md
 @del dist\windows\LICENSE.md
 @rmdir /Q /S dist\windows\libs
-@del dist\win32\cambridge.exe
+@del dist\win32\stackfuse.exe
 @del dist\win32\SOURCES.md
 @del dist\win32\LICENSE.md
 @rmdir /Q /S dist\win32\libs
-@del dist\cambridge-windows.zip
-@del dist\cambridge-win32.zip
+@del dist\other\stackfuse.love
+@del dist\other\SOURCES.md
+@del dist\other\LICENSE.md
+@rmdir /Q /S dist\other\libs
+@rmdir /Q /S dist\other
+@del dist\stackfuse-windows.zip
+@del dist\stackfuse-win32.zip
+@del dist\stackfuse-other.zip
--- release.bat
+++ release.bat
@@ -5,17 +5,23 @@ mkdir dist\windows
 mkdir dist\windows\libs
 mkdir dist\win32
 mkdir dist\win32\libs
+mkdir dist\other
+mkdir dist\other\libs
 
 copy /b dist\windows\love.exe+stackfuse.love dist\windows\stackfuse.exe
 copy /b dist\win32\love.exe+stackfuse.love dist\win32\stackfuse.exe
+copy /b stackfuse.love dist\other\stackfuse.love
 
 copy libs\discord-rpc.dll dist\windows\libs
 copy libs\discord-rpc.dll dist\win32\libs
+copy libs\discord-rpc.* dist\other\libs
 
 copy SOURCES.md dist\windows
 copy LICENSE.md dist\windows
 copy SOURCES.md dist\win32
 copy LICENSE.md dist\win32
+copy SOURCES.md dist\other
+copy LICENSE.md dist\other
 
 cd dist\windows
 tar -a -c -f ..\stackfuse-windows.zip stackfuse.exe *.dll libs *.md
@@ -23,4 +29,8 @@ cd ..\..
 
 cd dist\win32
 tar -a -c -f ..\stackfuse-win32.zip stackfuse.exe *.dll libs *.md
+cd ..\..
+
+cd dist\other
+tar -a -c -f ..\stackfuse-other.zip stackfuse.love libs *.md
 cd ..\..
